ad25547050f35a7f533733a1578263c1fca245e7
Kolonel1 <victor.munyao@student.moringaschool.com>
2022-08-09 22:36:18 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function logTwoValues(value1,value2){console.log(`the two values ${value1}and ${value2}.`);}
+//commas are necessary to list the parameters
+function introductionWithLanguage(name,language){return(`Hi, my name is ${name} and I am learning to program in ${language}.`);}
+
+//now i do not know what is up with this chrome shit it really is messing with me is it not now 
+
+function introductionWithLanguageOptional( name='victor',language='JavaScript'){return (`Hi, my name is ${name} and I am learning to program in ${language}.`);}
+introductionWithLanguageOptional(stupid,java)
+function introduction(name){return(`Hi, my name is ${name}.`);}
+
